141c847201b6bfa5e2dfce63b803f276ab5eb884
DermyHughes <dermot.s.hughes@gmail.com>
2022-02-28 16:54:42 +0000

    Change let to const for cv gen

--- src/utils/generate-pdf.js
+++ src/utils/generate-pdf.js
@@ -4,7 +4,7 @@ const puppeteer = require(`puppeteer`);
 
 (async () => {
     const browser = await puppeteer.launch();
-    let page = await browser.newPage();
+    const page = await browser.newPage();
     await page.goto(`https://dermothughes.com/cvraw/`); // Generate from a prod build
     await page.pdf({
         path: `./public/dermot-hughes-cv.pdf`, // path (relative to CWD) to save the PDF to.
