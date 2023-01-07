4ac5e9777db790c14ebcbd95cbe787767d85c0f7
DermyHughes <dermot.s.hughes@gmail.com>
2023-01-07 00:22:06 +0000

    Refactor build cv script

--- build-cv.js
+++ build-cv.js
@@ -3,17 +3,23 @@ const fs = require('fs');
 const { spawn } = require('child_process');
 const waitOn = require('wait-on');
 
-async function generatePdf() {
-  // The URL of the webpage you want to generate a PDF from
-  const url = 'http://localhost:9000/cvraw';
+// The URL of the webpage you want to generate a PDF from
+const PORT = '9001';
+const URL = `http://localhost:${PORT}/cvraw`;
 
+async function generatePdf() {
   // Set up Puppeteer
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.goto(URL, { waitUntil: 'networkidle2' });
 
   // Generate the PDF file
   const pdfBuffer = await page.pdf({ format: 'A4' });
+
+  // Confirm pdfBuffer does not just contain "Not found"
+  if (pdfBuffer.toString().includes('Not found')) {
+    throw new Error('PDF generation failed');
+  }
 
   // Write the PDF to a file in the 'public/exports' directory
   fs.writeFileSync(`${__dirname}/static/dermot-hughes-cv.pdf`, pdfBuffer);
@@ -24,11 +30,11 @@ async function generatePdf() {
 
 async function main() {
   // Start the Gatsby development server as a child process
-  const gatsby = spawn('gatsby', ['serve', '--port', '9000']);
+  const gatsby = spawn('gatsby', ['serve', '--port', PORT]);
 
   // Wait for the development server to start
   await waitOn({
-    resources: ['tcp:localhost:9000'],
+    resources: [`tcp:localhost:${PORT}`],
   });
 
   // Generate the PDF
@@ -36,6 +42,15 @@ async function main() {
 
   // Stop the Gatsby development server
   gatsby.kill();
+
+  // Wait for the development server to stop
+  await waitOn({
+    resources: [`tcp:localhost:${PORT}`],
+    reverse: true,
+  });
+
+  // Run gatsby build command
+  spawn('gatsby', ['build']);
 }
 
 main();
