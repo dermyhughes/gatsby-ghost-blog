987adfbd3bd04ab5ab8195c5d2f494514cb4b1ae
DermyHughes <dermot.s.hughes@gmail.com>
2023-03-24 21:42:30 +0000

    Change script

--- build-cv.js
+++ build-cv.js
@@ -1,65 +1,33 @@
+/* eslint-disable no-console */
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const waitOn = require('wait-on');
 
-// The URL of the webpage you want to generate a PDF from
-const URL = process.argv[2]; // URL passed as argument
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:9001/cv', {
+      waitUntil: 'networkidle2',
+    });
 
-async function generatePdf() {
-  // Set up Puppeteer
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(URL, {
-    waitUntil: 'networkidle2',
-    timeout: 120000, // 2 minutes
-  });
+    console.log('Generating the PDF...');
+    await page.pdf({
+      path: `${__dirname}/static/dermot-hughes-cv.pdf`,
+      format: 'A4',
+      printBackground: true,
+    });
 
-  // Check if the page has loaded
-  const pageLoaded = await page.evaluate(() => {
-    // Check if the page has the expected title
-    const expectedTitle = 'Dermot Hughes CV';
-    const actualTitle = document.title;
-    if (actualTitle !== expectedTitle) {
-      return false;
-    }
+    console.log('PDF generated successfully. Closing Puppeteer...');
+    await browser.close();
 
-    // Check if the page has the expected content
-    const expectedContent = 'Personal Profile';
-    const actualContent = document.body.innerText;
-    if (!actualContent.includes(expectedContent)) {
-      return false;
-    }
+    // Read server PID and kill the process
+    console.log('Shutting down the server...');
+    const serverPid = fs.readFileSync('server.pid', 'utf-8');
+    process.kill(serverPid);
+    fs.unlinkSync('server.pid');
 
-    return true;
-  });
-
-  if (!pageLoaded) {
-    throw new Error('Page did not load');
+    console.log('Server shut down successfully.');
+  } catch (error) {
+    console.error('An error occurred during the PDF generation process:', error);
   }
-
-  // Generate the PDF file
-  const pdfBuffer = await page.pdf({ format: 'A4' });
-
-  // Confirm pdfBuffer does not just contain "Not found"
-  if (pdfBuffer.toString().includes('Not found')) {
-    throw new Error('PDF generation failed');
-  }
-
-  // Write the PDF to a file in the 'public/exports' directory
-  fs.writeFileSync(`${__dirname}/static/dermot-hughes-cv.pdf`, pdfBuffer);
-
-  // Close Puppeteer
-  await browser.close();
-}
-
-async function main() {
-  // Wait for the specified URL to become available
-  await waitOn({
-    resources: [URL],
-  });
-
-  // Generate the PDF
-  await generatePdf();
-}
-
-main();
+})();
