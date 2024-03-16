3b7f935d1d000d9a0d345767415d38ed9011007e
DermyHughes <dermot.s.hughes@gmail.com>
2024-03-16 21:08:54 +0000

    Remove author code

--- src/components/common/PostCard.jsx
+++ src/components/common/PostCard.jsx
@@ -31,25 +31,7 @@ function PostCard({ post }) {
       </header>
       <section className='post-card-excerpt'>{post.excerpt}</section>
       <footer className='post-card-footer'>
-        <div className='post-card-footer-left'>
-          <div className='post-card-avatar'>
-            {post.primary_author.profile_image ? (
-              <img
-                className='author-profile-image'
-                src={post.primary_author.profile_image}
-                alt={post.primary_author.name}
-              />
-            ) : (
-              <img
-                className='default-avatar'
-                src='/images/icons/avatar.svg'
-                alt={post.primary_author.name}
-              />
-            )}
-          </div>
-          <span>{post.primary_author.name}</span>
-        </div>
-        <div className='post-card-footer-right'>
+        <div className='reading-time'>
           <div>{readingTime}</div>
         </div>
       </footer>
@@ -72,10 +54,6 @@ PostCard.propTypes = {
       }),
     ),
     excerpt: PropTypes.string.isRequired,
-    primary_author: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      profile_image: PropTypes.string,
-    }).isRequired,
   }).isRequired,
 };
 
